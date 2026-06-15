56e34ce48423a4336056088be2e7b3a6fb6b3d52
kasiazuchowska <134449853+kasiazuchowska@users.noreply.github.com>
2026-06-15 11:00:41 +0200

    Update my_playbook.py

--- playbooks/my_playbook.py
+++ playbooks/my_playbook.py
@@ -26,7 +26,7 @@ def geolocate_ip_1(action=None, success=None, container=None, results=None, hand
     parameters = []
 
     parameters.append({
-        "ip": "5.5.5.5",
+        "ip": "6.6.6.6",
     })
 
     ################################################################################
